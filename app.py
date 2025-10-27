9c418aa9b16333e02473661386327f7f7035b8f0
JIMIL MITULKUMAR PATEL <jimpatel2564@gmail.com>
2025-10-27 13:13:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,6 @@ def media_file(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
